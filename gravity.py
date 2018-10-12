618d67b1c5606d230f5125deb115d60b78629da4
vincentlee1218 <44049108+vincentlee1218@users.noreply.github.com>
2018-10-12 09:02:20 -0700

    substitute variables
    Getting ahead of the game by substituting variables in the text

--- gravity.py
+++ gravity.py
@@ -3,21 +3,30 @@
 
 # Oct-12-2018
 
+GRAVITY_EARTH=9.8
+TIME=0.6
+VELOCITY_0=16
+HEIGHT_0=16
+TIMEUNIT_LONG="second"
+TIMEUNIT_PLUR="seconds"
+TIMEUNIT_SHORT="sec"
+LENGTHUNIT_LONG="feet"
+LENGTHUNIT_PLUR="feet"
+LENGTHUNIT_SHORT="ft"
 
 def calcFallTime(t, v, h):
-    h=-16*t*t+v*t+h
+    h=-GRAVITY_EARTH/2*t*t+v*t+h
     return h
-
 
 print "Calculate the height you will achieve"
 
-time=0.6 
-velocity=16
-height=16
+time=TIME
+velocity=VELOCITY_0
+height=HEIGHT_0
 
 final=calcFallTime(time, velocity, height)
-print "After 1.6 seconds with a starting"
-print "velocity of 16 ft/sec and a starting"
-print "height of 16 feet"
+print "After "+str(TIME)+" "+TIMEUNIT_SHORT+" with a starting"
+print "velocity of "+str(HEIGHT_0)+" "+LENGTHUNIT_SHORT+"/"+TIMEUNIT_SHORT+" and a starting"
+print "height of "+str(HEIGHT_0)+" "+LENGTHUNIT_PLUR
 print ""
-print "You will achieve "+str(final)+" feet"
+print "You will achieve "+str(final)+" "+LENGTHUNIT_PLUR
