d4b8d9123061064f33375d5e8b40d0e5da975174
vincentlee1218 <44049108+vincentlee1218@users.noreply.github.com>
2019-05-02 22:32:36 -0700

    Image renderer
    As our class progresses through this bonus unit that is not covered in the CSP syllabus, new programs with different variations will be released. Stay tuned.

---
+++ 1.4.2.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+'''
+JDoe_JSmith_1_4_2: Read and show an image.
+'''
+import matplotlib.pyplot as plt
+import os.path
+import numpy as np # “as” lets us use standard abbreviations
+   
+'''Read the image data'''
+# Get the directory of this python script
+directory = os.path.dirname(os.path.abspath(__file__)) 
+# Build an absolute filename from directory + filename
+filename = os.path.join(directory, 'woman.jpg')
+# Read the image data into an array
+img = plt.imread(filename)
+  
+'''Show the image data'''
+# Create figure with 1 subplot
+fig, ax = plt.subplots(1, 1)
+# Show the image data in a subplot
+ax.imshow(img, interpolation='none')
+# Show the figure on the screen
+fig.show()
